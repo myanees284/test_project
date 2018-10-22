3ca2767a06e9bde3c91f4b521f4fa9f071e1c84c
Anees Mohammed <aneesm@ae.ibm.com>
2018-10-22 13:18:40 +0400

    dddd

--- hellotest/src/hellotest/helloworld.java
+++ hellotest/src/hellotest/helloworld.java
@@ -4,7 +4,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-for (int i = 0; i < 9; i++) {
+for (int i = 0; i < 7; i++) {
 	System.out.println("hello world"+i);
 }
 	}
