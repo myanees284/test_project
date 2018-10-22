0df046cc62a1adf1715fecf1ba9832f187d4e21f
Anees Mohammed <aneesm@ae.ibm.com>
2018-10-22 14:56:32 +0400

    at the rate

--- hellotest/src/hellotest/helloworld.java
+++ hellotest/src/hellotest/helloworld.java
@@ -5,7 +5,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 for (int i = 0; i < 10; i++) {
-	System.out.println("Life sucks"+i);
+	System.out.println("Life sucks @"+i);
 }
 	}
 
